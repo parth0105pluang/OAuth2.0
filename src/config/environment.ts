ca54e7c6ded183cd3fededbd6eb6dc23f3d12dcd
Parth Sharma <parth.sharma@pluang.com>
2022-03-09 12:20:43 +0530

    evironment interface

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,6 +1,14 @@
-import * as Joi from '@hapi/joi';
+import * as Joi from 'joi';
 
 import logger from '../helpers/logger';
+
+interface Environment {
+    MONGO_ATLAS: string;
+    EMAIL_USERNAME: string;
+    EMAIL_PASSWORD: string;
+    USER_VERIFICATION_TOKEN_SECRET: string;
+}
+
 const schema = Joi.object({
     MONGO_ATLAS: Joi.string().uri()
         .required(),
@@ -8,21 +16,21 @@ const schema = Joi.object({
         .required(),
     EMAIL_PASSWORD: Joi.string().required(),
     USER_VERIFICATION_TOKEN_SECRET: Joi.string().required(),
-}).options({ stripUnknown: true,convert: true,abortEarly: false, });
-let envi;
-try {
-    const value = schema.validate({ MONGO_ATLAS: process.env.MONGO_ATLAS, USER_VERIFICATION_TOKEN_SECRET:process.env.USER_VERIFICATION_TOKEN_SECRET,EMAIL_USERNAME:process.env.EMAIL_USERNAME,EMAIL_PASSWORD: process.env.EMAIL_PASSWORD})
-    envi=value.value;
-    logger.info(value);
-}
-catch (err) { 
-    logger.info(err);
-}
+}).options({ stripUnknown: true, convert: true, abortEarly: false });
 
-/*const envi= {
-    MONGO_ATLAS: process.env.MONGO_ATLAS,
-    EMAIL_USERNAME: process.env.EMAIL_USERNAME,
-    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
-    USER_VERIFICATION_TOKEN_SECRET: process.env.USER_VERIFICATION_TOKEN_SECRET
-};*/
+let envi: Environment;
+
+try {
+    const value = schema.validate({
+        MONGO_ATLAS: process.env.MONGO_ATLAS,
+        USER_VERIFICATION_TOKEN_SECRET: process.env.USER_VERIFICATION_TOKEN_SECRET,
+        EMAIL_USERNAME: process.env.EMAIL_USERNAME,
+        EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+    });
+    envi = value.value;
+    logger.info(value);
+} catch (err) {
+    logger.info(err);
+    throw err;
+}
 export default envi;
